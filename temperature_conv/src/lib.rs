f16f260cd54ecceccdebf4284b0481157417b713
aelkarou <karoumi14@gmail.com>
2024-07-01 16:35:58 +0200

    conv_temp

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,9 +1,11 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    round_to_n_decimal_places ( (f - 32.0) * (5.0 / 9.0),2)
+    let multiplier = 10f64.powi(2i32);
+     (((f - 32.0) * (5.0 / 9.0))*multiplier).round()/multiplier
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-   round_to_n_decimal_places ((c * (9.0 / 5.0)) + 32.0, 2)
+    let multiplier = 10f64.powi(2i32);
+    (((c * (9.0 / 5.0)) + 32.0)*multiplier).round()/multiplier
 }
 
 pub fn round_to_n_decimal_places(value: f64, n: u32) -> f64 {
