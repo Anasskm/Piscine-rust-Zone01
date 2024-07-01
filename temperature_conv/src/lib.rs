dafa2a279cc6bbf8639aca2bc387379b9fba70d1
aelkarou <karoumi14@gmail.com>
2024-07-01 20:23:23 +0200

    temp

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,9 +1,7 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    let multiplier = 10f64.powi(2i32);
-     (((f - 32.0) * (5.0 / 9.0))*multiplier).round()/multiplier
+    (f - 32.0) /(1.8)
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    let multiplier = 10f64.powi(2i32);
-    (((c * (9.0 / 5.0)) + 32.0)*multiplier).round()/multiplier
+    (c * 1.8) + 32.0
 }
