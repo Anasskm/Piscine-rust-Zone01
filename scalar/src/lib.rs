afecbfbb779523d6659f2c3fa529aa45096a0201
aelkarou <karoumi14@gmail.com>
2024-07-01 16:00:28 +0200

    scalar

---
+++ scalar/src/lib.rs
@@ -0,0 +1,20 @@
+pub fn sum(a: u8, b: u8) -> u8 {
+    a + b
+}
+
+pub fn diff(a: i16, b: i16) -> i16 {
+    a - b
+}
+
+pub fn pro(a: i8, b: i8) -> i8 {
+    a * b
+}
+
+
+pub fn quo(a: f32, b: f32) -> f32 {
+    a/b
+}
+
+pub fn rem(a: f32, b: f32) -> f32 {
+    a%b
+}
