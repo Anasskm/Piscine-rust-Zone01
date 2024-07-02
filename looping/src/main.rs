39995ce79420c9345200883056928cdecdf6bfb5
aelkarou <karoumi14@gmail.com>
2024-07-02 11:52:39 +0200

    looping

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     let mut count = 0;
     let answer = "The letter e";
     loop {
-        
+        count = count + 1;
 
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
         let mut input = String::new();
@@ -17,7 +17,7 @@ fn main() {
                 break;
             }
             _ => {
-                count = count + 1;
+                continue;
             }
         }
     }
