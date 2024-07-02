91c1298b71b148617f0074c48d1ad3dadb200186
aelkarou <karoumi14@gmail.com>
2024-07-02 11:49:55 +0200

    looping

---
+++ looping/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    use std::io;
+    let mut count = 0;
+    let answer = "The letter e";
+    loop {
+        
+
+        println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let input = input.trim();
+        match input {
+            s if s == answer => {
+                println!("Number of trials: {}", count);
+                break;
+            }
+            _ => {
+                count = count + 1;
+            }
+        }
+    }
+}
