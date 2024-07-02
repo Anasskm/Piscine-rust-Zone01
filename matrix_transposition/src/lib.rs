6f2f84959437abbdcaf89edf790dc14bfeb32a6a
aelkarou <karoumi14@gmail.com>
2024-07-02 15:22:37 +0200

    matrix

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,6 +1,6 @@
 #[derive(Debug)]
 
-pub struct Matrix((i32, i32), (i32, i32));
+pub struct Matrix(pub (i32, i32), pub (i32, i32));
 
 pub fn transpose(m: Matrix) -> Matrix {
     let res = Matrix(((m.0).0, (m.1).0), ((m.0).1, (m.1).1));
