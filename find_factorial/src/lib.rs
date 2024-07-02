b5410eb6aa5469fdfaef26811b5eedd308181e1c
aelkarou <karoumi14@gmail.com>
2024-07-02 14:48:52 +0200

    find_factorial

--- find_factorial/src/lib.rs
+++ find_factorial/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn factorial(num: u64) -> u64 {
+    if num == 0 {
+        return 1;
     }
+    if num == 1 {
+       return  1;
+    }
+
+    return num*factorial(num-1)
 }
