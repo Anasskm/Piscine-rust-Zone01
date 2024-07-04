07c3922a3ea21032f67a8fbe5ce2d2c60cd7de31
aelkarou <karoumi14@gmail.com>
2024-07-04 10:43:07 +0200

    arrays

---
+++ arrays/src/lib.rs
@@ -0,0 +1,13 @@
+pub fn sum(a:&[i32]) -> i32 {
+    let mut res:i32 = 0;
+    for e in a {
+        res += e
+    }
+    res 
+
+
+}
+
+pub fn thirtytwo_tens() -> [i32; 32] {
+    [10 as i32; 32]
+}
