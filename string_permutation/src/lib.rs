395b5fcb51abc507212023afc7b67ee9236c008f
aelkarou <karoumi14@gmail.com>
2024-07-04 15:19:15 +0200

    string_permutation

---
+++ string_permutation/src/lib.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+
+pub fn is_permutation(s1: &str, s2: &str) -> bool {
+    let mut s1_map = HashMap::new();
+    let mut s2_map = HashMap::new();
+    for s in s1.chars().into_iter() {
+        let count = s1_map.entry(s).or_insert(0);
+        *count += 1
+    }
+    for s in s2.chars().into_iter() {
+        let count = s2_map.entry(s).or_insert(0);
+        *count += 1
+    }
+    for (key, value) in s1_map {
+        if  !s2_map.contains_key(&key) ||s2_map[&key] != value {
+            return false;
+        }
+    }
+    true
+}
