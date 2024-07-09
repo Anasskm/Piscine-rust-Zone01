48353f9bd6cee322cf42798dd53ba6d784c7afa0
aelkarou <karoumi14@gmail.com>
2024-07-09 14:52:00 +0200

    file handling

---
+++ handling/src/lib.rs
@@ -0,0 +1,19 @@
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write};
+
+pub fn open_or_create(file: &str, content: &str) {
+
+    let mut file_handle = match OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(file)
+    {
+        Ok(file) => file,
+        Err(err) => panic!("{}", err),
+    };
+
+    if let Err(err) = file_handle.write_all(content.as_bytes()) {
+        panic!("{}", err);
+    }
+}
