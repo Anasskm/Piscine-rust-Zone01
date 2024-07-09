5c9fb7657ae33f33a6a24fc308bbfba55b9484bc
aelkarou <karoumi14@gmail.com>
2024-07-09 13:58:11 +0200

    second

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -8,22 +8,10 @@ pub enum Security {
 
 pub fn fetch_data(server: Result<String, String>, security_level: Security) -> String {
     match security_level {
-        Security::Unknown => {
-            server.expect("Unknown security level panic");
-            unreachable!()
-        }
-        Security::High => {
-            server.expect("ERROR: program stops");
-            unreachable!()
-        }
-        Security::Medium => {
-            server.unwrap_or("WARNING: check the server".to_string())
-        }
-        Security::Low => {
-            server.unwrap_or_else(|err| format!("Not found: {}", err))
-        }
-        Security::BlockServer => {
-            server.unwrap_err()
-        }
+        Security::Unknown => server.unwrap(),
+        Security::High => server.expect("ERROR: program stops"),
+        Security::Medium => server.unwrap_or("WARNING: check the server".to_string()),
+        Security::Low => server.unwrap_or_else(|err| format!("Not found: {}", err)),
+        Security::BlockServer => server.unwrap_err(),
     }
 }
