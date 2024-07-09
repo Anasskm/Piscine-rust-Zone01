fdda72a30900f0d9f160abb8245bc88d1d9fcc6f
aelkarou <karoumi14@gmail.com>
2024-07-09 14:14:44 +0200

    panic

--- panic/src/lib.rs
+++ panic/src/lib.rs
@@ -1,6 +1,8 @@
-use std::fs;
 use std::fs::File;
 
 pub fn open_file(s: &str) -> File {
-    File::open(s).unwrap()
+    match File::open(s) {
+        Ok(f) => f,
+        Err(_) => panic!("{}", "File not found"),
+    }
 }
