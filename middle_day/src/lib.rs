8ed9fe626623ca3fa704f50a005238ce660f316e
aelkarou <karoumi14@gmail.com>
2024-07-10 13:29:33 +0200

    middle day

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,6 +1,6 @@
 use chrono::{Datelike, NaiveDate, Weekday};
 
-fn middle_day(year: i32) -> Option<Weekday> {
+pub fn middle_day(year: i32) -> Option<Weekday> {
     let start = NaiveDate::from_ymd_opt(year, 1, 1)?;
     let end = NaiveDate::from_ymd_opt(year + 1, 1, 1)?;
     let days = end.signed_duration_since(start).num_days() as u32;
