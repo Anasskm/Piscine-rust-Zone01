1c82bd7da71cf2921b42c0a4adbd034b008824ac
aelkarou <karoumi14@gmail.com>
2024-07-10 11:33:58 +0200

    boxing error

---
+++ boxing_todo/src/err.rs
@@ -0,0 +1,45 @@
+use std::error::Error;
+use std::fmt;
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub enum ParseErr {
+    // expected public fields
+    Empty,
+    Malformed(Box<dyn Error>),
+}
+
+// required by error trait
+impl Display for ParseErr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Fail to parse todo")
+    }
+}
+
+#[derive(Debug)]
+pub struct ReadErr {
+    // expected public fields
+    pub child_err: Box<dyn Error>,
+}
+
+// required by error trait
+impl Display for ReadErr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Fail to read todo file")
+    }
+}
+
+impl Error for ParseErr {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            ParseErr::Empty => None,
+            ParseErr::Malformed(e) => Some(e.as_ref()),
+        }
+    }
+}
+
+impl Error for ReadErr {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        Some(self.child_err.as_ref())
+    }
+}
---
+++ boxing_todo/src/lib.rs
@@ -0,0 +1,62 @@
+mod err;
+use err::{ParseErr, ReadErr};
+
+pub use json::{parse, stringify};
+pub use std::error::Error;
+use std::{fs::File, io::Read};
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Task {
+    pub id: u32,
+    pub description: String,
+    pub level: u32,
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct TodoList {
+    pub title: String,
+    pub tasks: Vec<Task>,
+}
+
+impl TodoList {
+    pub fn get_todo(path: &str) -> Result<TodoList, Box<dyn Error>> {
+        let mut file = File::open(path).map_err(|e| {
+            Box::new(ReadErr {
+                child_err: Box::new(e),
+            }) as Box<dyn Error>
+        })?;
+        let mut s = String::new();
+        file.read_to_string(&mut s).map_err(|e| {
+            Box::new(ReadErr {
+                child_err: Box::new(e),
+            }) as Box<dyn Error>
+        })?;
+        if s.trim().is_empty() {
+            return Err(Box::new(ParseErr::Empty));
+        }
+        let parsed_json =
+            parse(&s).map_err(|e| Box::new(ParseErr::Malformed(Box::new(e))) as Box<dyn Error>)?;
+        let title = parsed_json["title"]
+            .as_str()
+            .ok_or_else(|| Box::new(ParseErr::Empty))?
+            .to_string();
+
+        let mut tasks: Vec<Task> = Vec::new();
+        for i in 0..parsed_json["tasks"].len() {
+            let task = Task {
+                id: parsed_json["tasks"][i]["id"]
+                    .as_u32()
+                    .ok_or_else(|| Box::new(ParseErr::Empty))?,
+                description: parsed_json["tasks"][i]["description"]
+                    .as_str()
+                    .ok_or_else(|| Box::new(ParseErr::Empty))?
+                    .to_string(),
+                level: parsed_json["tasks"][i]["level"]
+                    .as_u32()
+                    .ok_or_else(|| Box::new(ParseErr::Empty))?,
+            };
+            tasks.push(task)
+        }
+        Ok(TodoList { title, tasks })
+    }
+}
