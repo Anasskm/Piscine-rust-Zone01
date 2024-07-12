080284c025c00dfe43d78eeae9633934dea9488d
aelkarou <karoumi14@gmail.com>
2024-07-12 11:19:07 +0200

    mobs first commit

---
+++ mobs/src/mobs.rs
@@ -0,0 +1,64 @@
+pub mod boss;
+pub mod member;
+
+pub use boss::Boss;
+pub use member::Member;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Mob {
+    pub name: String,
+    pub boss: Boss,
+    pub members: Vec<Member>,
+    pub cities: Vec<(String, u8)>,
+    pub wealth: u32,
+}
+
+impl Mob {
+    pub fn recruit(&mut self, name: String, age: u8) {
+        self.members
+            .push(Member::new(name, member::Role::Associate, age))
+    }
+    pub fn attack(&mut self, mob: &mut Mob) {
+        let pw_self: u8 = self.members.iter().map(|m| m.role.power()).sum();
+        let pw_mob: u8 = mob.members.iter().map(|m| m.role.power()).sum();
+        if pw_self > pw_mob {
+            mob.members.pop();
+            if mob.members.len() == 0 {
+                self.wealth += mob.wealth;
+                mob.wealth = 0;
+                while mob.cities.len() > 0 {
+                    self.cities.push(mob.cities[0].clone());
+                    mob.cities.remove(0);
+                }
+            }
+            return;
+        }
+        self.members.pop();
+        if self.members.len() == 0 {
+            mob.wealth += self.wealth;
+            self.wealth = 0;
+            while self.cities.len() > 0 {
+                mob.cities.push(self.cities[0].clone());
+                self.cities.remove(0);
+            }
+        }
+    }
+    pub fn steal(&mut self, mob: &mut Mob, value: u32) {
+        if mob.wealth > value {
+            mob.wealth -= value;
+            self.wealth += value;
+            return;
+        }
+        self.wealth += mob.wealth;
+        mob.wealth = 0;
+    }
+    pub fn conquer_city(&mut self, mobs: Vec<&Mob>, city: String, value: u8) {
+        let have = mobs
+            .iter()
+            .flat_map(|&m| m.cities.clone())
+            .any(|(e, _)| e == city);
+        if !have {
+            self.cities.push((city, value))
+        }
+    }
+}
---
+++ mobs/src/mobs/member.rs
@@ -0,0 +1,38 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct Member {
+    pub name: String,
+    pub role: Role,
+    pub age: u8,
+}
+
+#[derive(Debug, Clone, PartialEq,Copy)]
+pub enum Role {
+    Underboss,
+    Caporegime,
+    Soldier,
+    Associate,
+}
+
+impl Member {
+    pub fn new(name: String, role: Role, age: u8) -> Member {
+        Member { name, role, age }
+    }
+    pub fn get_promotion(&mut self) {
+        match self.role {
+            Role::Associate => self.role = Role::Soldier,
+            Role::Soldier => self.role = Role::Caporegime,
+            Role::Caporegime => self.role = Role::Underboss,
+            _ => {}
+        }
+    }
+}
+impl Role {
+    pub fn power(&self) -> u8 {
+        match self {
+            Role::Associate => 1,
+            Role::Soldier => 2,
+            Role::Caporegime => 3,
+            Role::Underboss => 4,
+        }
+    }
+}
