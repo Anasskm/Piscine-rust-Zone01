ed3eab0d51aacf2db976c32e1bd6a4885cc9ef0d
aelkarou <karoumi14@gmail.com>
2024-07-17 11:29:35 +0200

    lifetimes

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+pub struct Person<'a> {
+    pub name: &'a str,
+    pub age: u8,
+}
+
+impl<'a> Person<'a> {
+    pub fn new(name: &'a str) -> Person<'a> {
+        Person {
+            name,
+            age: 0,
+        }
+    }
+}
+
