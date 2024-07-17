5b6ebbcc157bb568c8c020d621a08b397f335f23
aelkarou <karoumi14@gmail.com>
2024-07-17 15:29:28 +0200

    matrix_ops

---
+++ matrix_ops/src/lib.rs
@@ -0,0 +1,72 @@
+use std::ops::{Add, Sub};
+
+use lalgebra_scalar::*;
+
+#[derive(Debug, Clone)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl<T: Scalar + std::clone::Clone> Matrix<T> {
+    pub fn new() -> Matrix<T> {
+        Matrix(vec![vec![T::zero()]])
+    }
+
+    pub fn zero(row: usize, col: usize) -> Matrix<T> {
+        Matrix(vec![vec![T::zero(); col]; row])
+    }
+
+    pub fn identity(n: usize) -> Matrix<T> {
+        let mut mat = vec![vec![T::zero(); n]; n];
+        for i in 0..n {
+            mat[i][i] = T::one();
+        }
+        Matrix(mat)
+    }
+}
+
+impl<T: Scalar> Sub for Matrix<T> {
+    type Output = Option<Matrix<T>>;
+
+    fn sub(self, other: Matrix<T>) -> Option<Matrix<T>> {
+        if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
+            return None;
+        }
+
+        let result = self
+            .0
+            .iter()
+            .zip(other.0.iter())
+            .map(|(row1, row2)| {
+                row1.iter()
+                    .zip(row2.iter())
+                    .map(|(&val1, &val2)| val1 - val2)
+                    .collect()
+            })
+            .collect();
+
+        Some(Matrix(result))
+    }
+}
+
+impl<T: Scalar> Add for Matrix<T> {
+    type Output = Option<Matrix<T>>;
+
+    fn add(self, other: Matrix<T>) -> Option<Matrix<T>> {
+        if self.0.len() != other.0.len() || self.0[0].len() != other.0[0].len() {
+            return None;
+        }
+
+        let result = self
+            .0
+            .iter()
+            .zip(other.0.iter())
+            .map(|(row1, row2)| {
+                row1.iter()
+                    .zip(row2.iter())
+                    .map(|(&val1, &val2)| val1 + val2)
+                    .collect()
+            })
+            .collect();
+
+        Some(Matrix(result))
+    }
+}
