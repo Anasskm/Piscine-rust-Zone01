976e77ac5f01220419d700804681ca667c71d8f2
aelkarou <karoumi14@gmail.com>
2024-07-17 15:02:49 +0200

    MAtrix

---
+++ matrix/src/lib.rs
@@ -0,0 +1,24 @@
+use lalgebra_scalar::*;
+
+// Assuming the Scalar trait is defined as follows:
+
+#[derive(Debug, Clone)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl<T: Scalar + std::clone::Clone> Matrix<T> {
+    pub fn new() -> Matrix<T> {
+        Matrix(vec![vec![T::zero()]])
+    }
+
+    pub fn zero(row: usize, col: usize) -> Matrix<T> {
+        Matrix(vec![vec![T::zero(); col]; row])
+    }
+
+    pub fn identity(n: usize) -> Matrix<T> {
+        let mut mat = vec![vec![T::zero(); n]; n];
+        for i in 0..n {
+            mat[i][i] = T::one();
+        }
+        Matrix(mat)
+    }
+}
